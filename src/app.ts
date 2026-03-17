736bbd1327f38302104cb70bc748b2f4dab52de9
RomanBalabaev <roman.balabaev@via-mobi.com>
2026-03-18 01:50:55 +0300

    create server

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,19 @@
-import { createUsersTable } from '@/models/CreateTables'
+const express = require('express')
+const cors = require('cors');
+const app = express()
+const port = 3000
 
-createUsersTable()
+app.use(cors());
+
+app.get('/', (req: any, res: any) => {
+  res.send('Hello World!')
+})
+
+
+app.get('/test', (req: any, res: any) => {
+  res.send('test test')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
