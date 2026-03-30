a5b46ffd5da7d13eec0f8fb259bc5136d7353384
RomanBalabaev <roman.balabaev@via-mobi.com>
2026-03-31 01:23:29 +0300

    add helper for test user

--- src/routers/User.test.ts
+++ src/routers/User.test.ts
@@ -1,49 +1,28 @@
 import request from 'supertest'
 import app from '@/app'
 import { pool } from '@/config/DatabasePool'
-import { afterAll, beforeAll, describe, expect, test } from '@jest/globals';
+import { beforeAll, describe, expect, test } from '@jest/globals';
 import { hash } from 'bcryptjs';
 import { config } from '@/config/env';
+import { getAuthToken } from '@/helpers/tests/Auth' 
+import { TestUser } from '@/types/tests'
 
 describe('User routes Integration Tests', () => {
-    let authToken: string
-    let userId: number
-    const userData = {
-        name: 'Billy Jean',
-        login: 'billy_jean',
-        email: 'billy@example.com',
-        password: 'SecurePass12345!'
-    };
+    let currentUser: TestUser 
     beforeAll(async () => {
-        const existingUser = await pool.query('SELECT user_id FROM users WHERE login = $1 LIMIT 1', [userData.login]);
-        if (existingUser.rowCount === 0) {
-            const passwordHash = await hash(userData.password, config.salt)
-            const createUserResult = await pool.query('INSERT INTO users (name, login, email, password) VALUES ($1, $2, $3, $4) RETURNING user_id, name, login, email', [userData.name, userData.login, userData.email, passwordHash])
-            userId = createUserResult.rows[0].user_id
-        } else {
-            userId = existingUser.rows[0].user_id
-        }
-        const loginResponse = await request(app)
-            .post('/api/login')
-            .send({
-                login: userData.login,
-                password: userData.password
-            });
-        
-        authToken = loginResponse.body.token;
-
+        currentUser = await getAuthToken();
     })
 
     describe('GET /user', () => {
         test('should get current user info', async () => {            
             const response = await request(app)
             .get('/user')
-            .set('Authorization', `Bearer ${authToken}`)
+            .set('Authorization', `Bearer ${currentUser.authToken}`)
             .expect(200);
             
             expect(response.body.user).toMatchObject({
-            name: userData.name,
-            user_id: userId
+            name: currentUser.name,
+            user_id: currentUser.userId
             });
         })
         
---
+++ src/types/tests.ts
@@ -0,0 +1,8 @@
+export interface TestUser {
+    name: string,
+    login: string,
+    email: string,
+    password: string,
+    userId: number,
+    authToken: string
+}
