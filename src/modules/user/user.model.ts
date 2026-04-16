4d603fdb3201490e39f291022c147b5610041d82
RomanBalabaev <roman.balabaev@via-mobi.com>
2026-04-16 23:16:37 +0300

    add method find user by id

--- src/modules/user/user.model.ts
+++ src/modules/user/user.model.ts
@@ -6,6 +6,13 @@ export const findUserByLogin = async (login: string)  => {
     return result.rows[0] || null;
 }
 
+export const findUserById = async (id: number)  => {
+    const query = 'SELECT * FROM users WHERE user_id = $1 LIMIT 1';
+    const result = await pool.query(query, [id]);
+    return result.rows[0] || null;
+}
+
+
 export const createUser = async (name: string, login: string, email: string, password: string)  => {
     const query = `
         INSERT INTO users (name, login, email, password) 
